c50ffe9c3a9dfbd9b31eb9963c8c1f5aba0123b5
thvvamshi <bodavamshikumar2002@gmail.com>
2026-05-02 02:29:28 +0530

    feat: fixed  deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,31 +1,32 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const [{ createApp }, { connectDb }, { env }] = await Promise.all([
-  import('./app.js'),
-  import('./config/db.js'),
-  import('./config/env.js')
+  import("./app.js"),
+  import("./config/db.js"),
+  import("./config/env.js"),
 ]);
 
 try {
   await connectDb();
   const app = createApp();
+  const PORT = process.env.PORT || env.PORT || 5000;
 
-  const server = app.listen(env.PORT, () => {
-    console.log(`API running on port ${env.PORT}`);
+  const server = app.listen(PORT, () => {
+    console.log(`API running on port ${PORT}`);
   });
 
   function shutdown(signal) {
     console.log(`${signal} received, shutting down`);
     server.close(() => {
-      console.log('HTTP server closed');
+      console.log("HTTP server closed");
       process.exit(0);
     });
   }
 
-  process.on('SIGTERM', () => shutdown('SIGTERM'));
-  process.on('SIGINT', () => shutdown('SIGINT'));
+  process.on("SIGTERM", () => shutdown("SIGTERM"));
+  process.on("SIGINT", () => shutdown("SIGINT"));
 } catch (error) {
   console.error(error);
   process.exit(1);
